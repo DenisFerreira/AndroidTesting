ec4b5ce483b32e91a972360335c7346e8dd0949d
DenisFerreira <denis_ferreira@outlook.com>
2021-03-30 22:13:11 -0300

    Denis first commit

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,50 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = false)
+        )
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 100f)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsHundredZero() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+        )
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(0f, result.completedTasksPercent)
+        assertEquals(0f, result.activeTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_error_returnsHundredZero() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = null
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertEquals(0f, result.completedTasksPercent)
+        assertEquals(0f, result.activeTasksPercent)
+    }
+}
